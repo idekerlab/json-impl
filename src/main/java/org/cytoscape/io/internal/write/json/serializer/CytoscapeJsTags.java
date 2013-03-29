1e466ff350c4c75b38c0aade4aebf1ce82586e9f
keiono <keiono@gmail.com>
2013-03-28 18:19:57 -0700

    Visual Style Mapping is partially done.  Need to implement Mapping converter.

--- src/main/java/org/cytoscape/io/internal/write/json/serializer/CytoscapeJsTags.java
+++
@@ -1,5 +0,0 @@
-package org.cytoscape.io.internal.write.json.serializer;
-
-public enum CytoscapeJsTags {
-
-}
--- src/main/java/org/cytoscape/io/internal/write/json/serializer/JsVisualStyleSerializer.java
+++ src/main/java/org/cytoscape/io/internal/write/json/serializer/JsVisualStyleSerializer.java
@@ -1,5 +1,8 @@
 package org.cytoscape.io.internal.write.json.serializer;
 
+import static org.cytoscape.io.internal.write.json.serializer.CytoscapeJsToken.*;
+
+import java.awt.Color;
 import java.io.IOException;
 
 import org.cytoscape.view.presentation.property.BasicVisualLexicon;
@@ -19,29 +22,70 @@ public class JsVisualStyleSerializer extends JsonSerializer<VisualStyle> {
 		jg.useDefaultPrettyPrinter();
 
 		jg.writeStartObject();
-		
+
 		// Title of Visual Style
-		jg.writeStringField("title", vs.getTitle());
-		
+		jg.writeStringField(TITLE.toString(), vs.getTitle());
+
 		// Mappings and Defaults are stored as array.
-		jg.writeArrayFieldStart("style");
-		
+		jg.writeArrayFieldStart(STYLE.toString());
+
 		// Node Mapping
-		jg.writeStartObject();
-		
-		jg.writeStringField("selector", "node");
-		
-		jg.writeObjectFieldStart("css");
-		jg.writeStringField("background-color", vs.getDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR).toString());
-		jg.writeEndObject();
-		
-		jg.writeEndObject();
-		
+		serializeNodeStyle(vs, jg);
+
+		// Edge Mapping
+		serializeEdgeStyle(vs, jg);
+
+		// TODO What else?
+
 		jg.writeEndArray();
-		
+
 		jg.writeEndObject();
 	}
-	
+
+	private void serializeNodeStyle(final VisualStyle vs, JsonGenerator jg) throws IOException {
+		jg.writeStartObject();
+		jg.writeStringField(SELECTOR.toString(), NODE.toString());
+
+		jg.writeObjectFieldStart(CSS.toString());
+		jg.writeStringField(BACKGROUND_COLOR.toString(),
+				decodeColor((Color) vs.getDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR)));
+		jg.writeStringField(BORDER_COLOR.toString(),
+				decodeColor((Color) vs.getDefaultValue(BasicVisualLexicon.NODE_BORDER_PAINT)));
+		jg.writeNumberField(BORDER_WIDTH.toString(), vs.getDefaultValue(BasicVisualLexicon.NODE_BORDER_WIDTH));
+		jg.writeStringField(SHAPE.toString(), vs.getDefaultValue(BasicVisualLexicon.NODE_SHAPE).getDisplayName()
+				.toLowerCase());
+		jg.writeEndObject();
+
+		jg.writeEndObject();
+	}
+
+	private void createMapping() {
+		// TODO implememt this
+		// Passthrough
+	}
+
+	private void serializeEdgeStyle(final VisualStyle vs, JsonGenerator jg) throws IOException {
+		jg.writeStartObject();
+		jg.writeStringField(SELECTOR.toString(), EDGE.toString());
+
+		jg.writeObjectFieldStart(CSS.toString());
+		jg.writeNumberField(WIDTH.toString(), vs.getDefaultValue(BasicVisualLexicon.EDGE_WIDTH));
+		jg.writeStringField(LINE_COLOR.toString(),
+				decodeColor((Color) vs.getDefaultValue(BasicVisualLexicon.EDGE_STROKE_UNSELECTED_PAINT)));
+		jg.writeEndObject();
+
+		jg.writeEndObject();
+	}
+
+	private final String decodeColor(Color color) {
+		final StringBuilder builder = new StringBuilder();
+
+		builder.append("#");
+		builder.append(Integer.toHexString(color.getRGB()));
+
+		return builder.toString();
+	}
+
 	@Override
 	public Class<VisualStyle> handledType() {
 		return VisualStyle.class;
